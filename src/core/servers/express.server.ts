fe9e95e01eb41e2b5c03bf9135a168538e364ce6
Jaime Salas Zancada <jaimesalaszancada@Jaimes-MacBook-Pro.local>
2021-10-09 12:03:30 +0200

    added code

---
+++ src/core/servers/express.server.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import cors from 'cors';
+import { envConstants } from 'core/constants';
+
+export const createApp = () => {
+  const app = express();
+  app.use(cors({ origin: envConstants.CORS_ORIGIN }));
+  // app.use(cors());
+  return app;
+};
